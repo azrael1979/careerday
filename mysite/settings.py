8f4b2a6e198148448d72acb5ad6923a59e280591
Matteo Tarantino <matteo.tarantino@unige.ch>
2018-09-20 16:15:01 +0200

    dfdf

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.environ["CAREER_SECRET_KEY"])
+SECRET_KEY = "aed!953ui*o%e3mhx4$9sk5l8zez@88!v(s%&lh0jot=7e#xo2" #os.environ["CAREER_SECRET_KEY"]
 #SECRET_KEY = os.environ['SECRET_KEY']
 
 
